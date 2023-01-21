2ea46770de9f8ef0cb40d1f6e708933e7f16db91
Guczu <jakub180720@gmail.com>
2023-01-21 23:42:41 +0100

    fixed bug with private messages

--- src/components/InboxMessage.jsx
+++ src/components/InboxMessage.jsx
@@ -5,20 +5,19 @@ import fetchOneUserById from '../methods/fetchOneUserById';
 
 export default function InboxMessage(props) {
     const { userId } = useParams();
-    const message = props.message;
     const [user, setUser] = useState(null);
 
     useEffect(() => {
         const getUser = async () => {
-            const user = await fetchOneUserById(message.data.user_id);
+            const user = await fetchOneUserById(props.message.data.user_id);
             setUser(user);
         }
         getUser();
-    }, [])
+    }, [props])
 
   return (
-    <div className={message.data.user_id === userId ? 'inboxmessage--containerleft' : 'inboxmessage--containerright'}>
-        <div className={message.data.user_id === userId ? 'inboxmessage--contentleft' : 'inboxmessage--contentright'}>
+    <div className={props.message.data.user_id === userId ? 'inboxmessage--containerleft' : 'inboxmessage--containerright'}>
+        <div className={props.message.data.user_id === userId ? 'inboxmessage--contentleft' : 'inboxmessage--contentright'}>
             <img src={user !== null ? user.picture : avatar}></img>
             <p className='inboxmessage--text'>{props.message.data.content}</p>
         </div>
